ee86516ef0e0d396b1a8075cb8be28e61be62d09
Sathish Kumar <124054894+SathishKumar369@users.noreply.github.com>
2025-10-19 13:32:57 +0530

    Data(Json Format)

---
+++ packagesData.js
@@ -0,0 +1,35 @@
+import bali from "../assets/bali.png";
+import paris from "../assets/paris.png";
+import dubai from "../assets/dubai.png";
+import maldives from "../assets/maldives.png";
+
+export const packagesData = [
+        {
+                   id: 1,
+                   image: bali,
+                   title: "Bali Getaway",
+                   price: "$899",
+                   days: "5 Days / 4 Nights",
+               },
+               {
+                   id: 2,
+                   image: paris,
+                   title: "Romantic Paris",
+                   price: "$1199",
+                   days: "6 Days / 5 Nights",
+               },
+               {
+                   id: 3,
+                   image: dubai,
+                   title: "Dubai Luxury Trip",
+                   price: "$999",
+                   days: "4 Days / 3 Nights",
+               },
+               {
+                   id: 4,
+                   image: maldives,
+                   title: "Maldives Escape",
+                   price: "$1299",
+                   days: "5 Days / 4 Nights",
+               },
+           ];
---
+++ testimonialsData.js
@@ -0,0 +1,28 @@
+import user1 from "../assets/user1.png";
+import user2 from "../assets/user2.png";
+import user3 from "../assets/user3.png";
+
+export const testimonialsData = [
+    
+            {
+                id: 1,
+                name: "Sophia Williams",
+                image: user1,
+                review:"TravelSphere made our honeymoon in pairs absolutely perfect. Everything was smooth and personalized. Highly recommended!",
+                rating: 5,
+            },
+            {
+                id: 2,
+                name: "James",
+                image: user2,
+                review:"Excellent service and great deals! They took care of everything for our Dubai vacation. Will book again.",
+                rating: 4,
+            },
+            {
+                id: 3,
+                name: "Mark Antony",
+                image: user3,
+                review:"The best travel consultancy I've ever used! They handled my visa and bookings without any hassle.",
+                rating: 5,
+            },
+        ];
